20be907c52c04378b10e1b8bf79b5766e4414119
Rana Hany <ranahany1003@gmail.com>
2026-03-27 14:02:19 +0200

    Add Lab 2: Implementation of Course List with Categories and ngClass/ngStyle

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CoursesComponent } from './components/courses/courses'; 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, CoursesComponent], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
