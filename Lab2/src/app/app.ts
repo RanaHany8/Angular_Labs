e96569b898ae11e319113e5dfd3bfee1953fd2b1
Rana Hany <ranahany1003@gmail.com>
2026-03-27 16:57:54 +0200

    Fix Lab2 folder visibility by removing internal .git

---
+++ Lab2/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CoursesComponent } from './components/courses/courses'; 
+
+@Component({
+  selector: 'app-root',
+  standalone: true, 
+  imports: [RouterOutlet, CoursesComponent], 
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('courses-app');
+}
